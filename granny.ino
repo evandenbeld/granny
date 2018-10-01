f0c9b1861a70636aecdf034f4491227c3b986261
erwin.van.den.beld <erwin.van.den.beld@topicus.nl>
2018-10-01 21:51:29 +0200

    ADSR using analog in (potentiometers)
    - Fixed led off
    - Update oscilators

--- granny.ino
+++ granny.ino
@@ -5,11 +5,12 @@
 #include <mozzi_midi.h>
 #include <ADSR.h>
 #include <mozzi_fixmath.h>
+#include <AutoMap.h>
 #include <tables/sin2048_int8.h>
 #include <tables/saw2048_int8.h>
 #include <tables/triangle2048_int8.h>
-#include <tables/square_no_alias_2048_int8.h>
-#include <tables/noise_static_1_16384_int8.h>
+#include <tables/square_analogue512_int8.h>
+#include <tables/brownnoise8192_int8.h>
 
 #define CONTROL_RATE 128
 
@@ -20,6 +21,11 @@
 #define PIN_TRIANGLE_WAVE_SWITCH 4
 #define PIN_SQUARE_WAVE_SWITCH 5
 #define PIN_NOISE_SWITCH 6
+
+#define ATTACK_PIN 0
+#define DECAY_PIN 1
+#define SUSTAIN_PIN 5
+#define RELEASE_PIN 2
 
 #define ATTACK 50
 #define DECAY 50
@@ -45,13 +51,15 @@ byte notes[] = {0, 0, 0, 0, 0, 0};
 byte gain[] = {0, 0, 0, 0, 0, 0};
 int sensorPin = A0;
 int decayValue = 50;
+unsigned int attack, decay, sustain, release_ms;
+AutoMap adsrKnobMapper(0, 1023, 50, 5000);
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 void updateLed() {
     int numberOfNotes = 0;
     for (int i = 0; i < NUMBER_OF_POLYPHONY; i++) {
-        numberOfNotes += notes[i];
+        if (notes[i] != 0) numberOfNotes++;
     }
 
     if (numberOfNotes == 0) {
@@ -68,7 +76,7 @@ void HandleNoteOn(byte channel, byte note, byte velocity) {
             notes[i] = note;
             oscillators[i].setFreq_Q16n16(Q16n16_mtof(Q8n0_to_Q16n16(note)));
             envelopes[i].noteOn();
-            break;
+            i = NUMBER_OF_POLYPHONY;
         }
     }
     updateLed();
@@ -82,7 +90,7 @@ void HandleNoteOff(byte channel, byte note, byte velocity) {
         if (note == notes[i]) {
             notes[i] = 0;;
             envelopes[i].noteOff();
-            break;
+            i = NUMBER_OF_POLYPHONY;
         }
     }
     updateLed();
@@ -116,7 +124,7 @@ void setup() {
 void updateWaveTable() {
 
 
-    WAVE_TABLE waveTable;
+    WAVE_TABLE waveTable = SINE;
     if (digitalRead(PIN_SINE_WAVE_SWITCH) == LOW) {
         waveTable = SINE;
     } else if (digitalRead(PIN_SAW_WAVE_SWITCH) == LOW) {
@@ -128,6 +136,7 @@ void updateWaveTable() {
     } else if (digitalRead(PIN_NOISE_SWITCH) == LOW) {
         waveTable = NOISE;
     }
+
 
     for (int i = 0; i < NUMBER_OF_POLYPHONY; i++) {
         switch (waveTable) {
@@ -141,10 +150,10 @@ void updateWaveTable() {
                 oscillators[i].setTable(SAW2048_DATA);
                 break;
             case SQUARE:
-                oscillators[i].setTable(SQUARE_NO_ALIAS_2048_DATA);
+                oscillators[i].setTable(SQUARE_ANALOGUE512_DATA);
                 break;
             case NOISE:
-                oscillators[i].setTable(NOISE_STATIC_1_16384_DATA);
+                oscillators[i].setTable(BROWNNOISE8192_DATA);
                 break;
         }
     }
@@ -155,9 +164,19 @@ void updateControl() {
 
     updateWaveTable();
 
+    int knob_value = mozziAnalogRead(ATTACK_PIN);
+    attack = adsrKnobMapper(knob_value);
+    knob_value = mozziAnalogRead(DECAY_PIN);
+    decay = adsrKnobMapper(knob_value);
+    knob_value = mozziAnalogRead(SUSTAIN_PIN);
+    sustain = adsrKnobMapper(knob_value);
+    knob_value = mozziAnalogRead(RELEASE_PIN);
+    release_ms = adsrKnobMapper(knob_value);
+
     MIDI.read();
 
     for (int i = 0; i < NUMBER_OF_POLYPHONY; i++) {
+        envelopes[i].setTimes(attack, decay, sustain, release_ms);
         envelopes[i].update();
         gain[i] = envelopes[i].next();
     }
